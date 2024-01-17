1acc6f8152024febc10cf1e9cda92eb4f70ffc7d
nhungdt-1833 <do.thi.nhung@sun-asterisk.com>
2024-01-17 13:59:31 +0700

    Quartz sync: Jan 17, 2024, 1:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "hatcaiday.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
